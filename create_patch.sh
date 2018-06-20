442bf95759af4515a1663d451369f66cae9e3faa
mundis <raimund.mathis@bluewin.ch>
2018-06-20 13:46:09 +0000

    better version create_patch.sh upload

--- create_patch.sh
+++ create_patch.sh
@@ -2,13 +2,13 @@
 
 STATUS=0
 if ! [ $3 ] || ! [ $2 ] || ! [ $1 ];then
-	echo -e "SYNTAX:create_patch folder1 folder2 patchname"
+	echo -e "SYNTAX:create_patch folder.orig folder.modified patchname"
 	STATUS=1
 fi
-if ! [ -d $1 ] || ! [ -d $2 ];then 
+if ! [ -d $1 ] || ! [ -d $2 ];then
 	echo -e "directories don't exist"
 	STATUS=1
 fi
 if ! [ $STATUS -eq 1 ];then
-	diff -crB $1 $2 > $3	
+	diff -Naru $1 $2 > $3
 fi
